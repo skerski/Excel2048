20910a1ba429802fa07c22a717ea5b191be56f92
skerski <qmaksim.maksimenko@gmail.com>
2021-05-18 15:19:40 +0300

    Finished basic table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,14 @@
-import { ExcelComponent } from "../../core/ExcelComponent";
+import { ExcelComponent } from "../../core/ExcelComponent"
 
 export class Header extends ExcelComponent {
-	static className = 'excel__header';
+	static className = "excel__header"
 
+	constructor($root, options) {
+		super($root, {
+			name: "Header",
+			...options,
+		})
+	}
 	toHTML() {
 		return `
 		<input type="text" class="input" value="New Table">
@@ -14,6 +20,6 @@ export class Header extends ExcelComponent {
 					<i class="material-icons">delete</i>
 				</div>
 			</div>
-		`;
+		`
 	}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,20 +1,43 @@
-import { DomListener } from '@core/DomListener';
+import { DomListener } from "@core/DomListener"
 
 export class ExcelComponent extends DomListener {
 	constructor($root, options = {}) {
 		super($root, options.listeners)
-		this.name = options.name || '';
+		this.name = options.name || ""
+		this.emitter = options.emitter
+		this.unsubcribers = []
+
+		this.prepare()
+	}
+
+	//returns component
+	prepare() {}
+
+	//returns pattern
+	toHTML() {
+		return ""
 	}
 	
-	toHTML() {
-		return ''
+	//notificate listener about event
+	$emit(event, ...args) {
+		this.emitter.emit(event, ...args)
 	}
 
+	$on(event, fn) {
+		const unsub = this.emitter.subscribe(event, fn)
+		this.unsubcribers.push(unsub)
+	}
+
+	//init component
+	//add DOMListeners
 	init() {
-		this.initDOMListeners();
+		this.initDOMListeners()
 	}
 
+	//remove components
+	//remove DOMListeners
 	destroy() {
-		this.resetDOMListeners();
+		this.resetDOMListeners()
+		this.unsubcribers.forEach(unsub => unsub())
 	}
 }
