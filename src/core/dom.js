334a2c0a5d636e7d11fb7c5cb1eb6152655ba364
skerski <qmaksim.maksimenko@gmail.com>
2021-05-22 13:50:57 +0300

    finished redux and table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,16 +14,16 @@ class Dom {
 	}
 
 	text(text) {
-		if (typeof text === 'string') {
+		if (typeof text !== "undefined") {
 			this.$el.textContent = text
 			return this
 		}
-		if (this.$el.tagName.toLowerCase() === 'input') {
+		if (this.$el.tagName.toLowerCase() === "input") {
 			return this.$el.value.trim()
 		}
 		return this.$el.textContent.trim()
 	}
-	
+
 	clear() {
 		this.html("")
 		return this
@@ -70,14 +70,21 @@ class Dom {
 	}
 
 	css(styles = {}) {
-		Object.keys(styles).forEach((key) => {
+		Object.keys(styles).forEach(key => {
 			this.$el.style[key] = styles[key]
 		})
 	}
 
+	getStyles(styles = []) {
+		return styles.reduce((res, s) => {
+			res[s] = this.$el.style[s]
+			return res
+		}, {})
+	}
+
 	id(parse) {
 		if (parse) {
-			const parsed = this.id().split(':')
+			const parsed = this.id().split(":")
 			return {
 				row: +parsed[0],
 				col: +parsed[1],
@@ -85,12 +92,20 @@ class Dom {
 		}
 		return this.data.id
 	}
-	
+
 	focus() {
 		this.$el.focus()
 		return this
 	}
-	
+
+	attr(name, value) {
+		if (value) {
+			this.$el.setAttribute(name, value)
+			return this
+		}
+		return this.$el.getAttribute(name)
+	}
+
 	addClass(className) {
 		this.$el.classList.add(className)
 		return this
